45ba12e83362efacccbc5400f28a9803c62f9281
Fazliddin <f.yuldashev.tuit@gmail.com>
2022-07-07 02:36:02 +0500

    done 1st page

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,4 +1,66 @@
-// Chart.defaults.scale.ticks.display = false;
+
+
+//map of Uzbekistan
+
+(async () => {
+
+  const topology = await fetch(
+      'https://code.highcharts.com/mapdata/countries/uz/uz-all.topo.json'
+  ).then(response => response.json());
+
+  // Prepare demo data. The data is joined to map using value of 'hc-key'
+  // property by default. See API docs for 'joinBy' for more info on linking
+  // data and map.
+  const data = [
+      ['uz-fa', 10], ['uz-tk', 11], ['uz-an', 12], ['uz-ng', 13],
+      ['uz-ji', 14], ['uz-si', 15], ['uz-ta', 16], ['uz-bu', 17],
+      ['uz-kh', 18], ['uz-qr', 19], ['uz-nw', 20], ['uz-sa', 21],
+      ['uz-qa', 22], ['uz-su', 23]
+  ];
+
+  // Create the chart
+  Highcharts.mapChart('map', {
+      chart: {
+          map: topology
+      },
+
+      title: {
+          text: ''
+      },
+
+      subtitle: {
+          text: ''
+      },
+
+      // mapNavigation: {
+      //     enabled: true,
+      //     buttonOptions: {
+      //         verticalAlign: 'bottom'
+      //     }
+      // },
+
+      // colorAxis: {
+      //     min: 0
+      // },
+
+      series: [{
+          data: data,
+          name: '',
+          states: {
+              hover: {
+                  color: '#001D84'
+              }
+          }
+          // dataLabels: {
+          //     enabled: false,
+          //     format: '{point.name}'
+          // }
+      }]
+  });
+
+})();
+
+// end map
 
     const data = {
       labels: ['Andijon', 'Buxoro', 'Jizzax', 'Qashqadaryo', 'Qoraqalpog’iston', 'Navoiy', 'Namangan', 'Samarqand', 'Sirdaryo', 'Surxondaryo', 'Toshkent vil.', 'Toshkent', 'Fargona', 'Xorazm'],
@@ -114,3 +176,73 @@
             maintainAspectRatio: false
         }
     });
+
+       //3nd chart
+
+
+       const ctx3 = document.getElementById('myChart3').getContext('2d');
+       const myChart3 = new Chart(ctx3, {
+           type: 'doughnut',
+           data: {
+               labels: ['VAQTINCHA', 'DOIMIY'],
+               datasets: [{
+                   label: 'HUQUQBUZARLIKLAR',
+                   data: [354359, 102957],
+                   backgroundColor: [
+                       '#2570D6',
+                       '#2FBD2C'
+                   ],
+                   borderWidth: 1,
+                   datalabels: {
+                    color: '#2B00D1',
+                    font: {
+                      weight: '500',
+                      size: 13,
+                    },
+                    anchor: 'end',
+                    align: 'top',
+                    offset: 10
+                  }
+               }]
+           },
+           plugins: [ChartDataLabels],
+           options: {
+               plugins: {
+                 legend: {
+                   display: false
+                 }
+               },
+               tooltips: {
+                   callbacks: {
+                     label: function(tooltipItem) {
+                   console.log(tooltipItem)
+                       return tooltipItem.yLabel;
+                   }
+                 }
+               },
+               scales: {
+                 y: {
+                  display: false,
+                   ticks: {
+                     display: false,
+                   },
+                   grid: {
+                     display: false
+                   }
+                 },
+                 x: {
+                  ticks: {
+                    display: false,
+                  },
+                   grid: {
+                     display: false
+                   }
+                 }
+               },
+               responsive: true,
+               maintainAspectRatio: false,
+               cutoutPercentage: 80,
+               rotation: -90,
+               circumference: 180
+           }
+       });
